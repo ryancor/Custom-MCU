b50c0daedce39feecf22217544079094d6030c6b
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-10 17:20:15 -0700

    modified to go into subdirs

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 # Compile
-echo "Starting compilation of ARM"
+echo "Starting compilation..."
 
-./gcc-arm/bin/arm-none-eabi-as startup.s -o startup.o
+./gcc-arm/bin/arm-none-eabi-as startup_chain/startup.s -o startup.o
 ./gcc-arm/bin/arm-none-eabi-gcc --specs=rdimon.specs   -Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group -c main.c -o main.o
-./gcc-arm/bin/arm-none-eabi-ld -T link.ld main.o startup.o -o main.elf
+./gcc-arm/bin/arm-none-eabi-ld -T linkers/link.ld main.o startup.o -o main.elf
 ./gcc-arm/bin/arm-none-eabi-objcopy -O binary main.elf main.bin
 
 echo "Compilation finished!"
 
-echo "Cleanup"
+echo "Cleanup time..."
 rm -rf *.o && rm -rf main.elf
 
 # Run
