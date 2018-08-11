d5138c86aa82b4cb152e6c61b37c43c228c2d42e
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-10 19:37:59 -0700

    adding data offsets

--- main.c
+++ main.c
@@ -2,14 +2,11 @@
 #include "io_headers/lpc13xx_stdio.h"
 
 int main(void) {
-  LPC_GPIO1->DIR |= (1 << 3);
-  // ldr	r2, [pc, #68] : r2 = 0x40012000
-  LPC_GPIO1->DATA |= (1 << 2);
-  // orr	r3, r3, #4 : r3 = 0x04
-  // str	r3, [r2] : stores with immediate offset 0x40012000 + 0x04
+  GPIO1_DIR->DIR |= (1 << 2); // set pin to output
+  GPIO1_DATA->DATA |= (1 << 1); // set to high
 
   char *data;
-  tochar(LPC_GPIO1->DATA, data);
+  tochar(GPIO1_DATA->DATA, data);
 
   print_uart0("LPC_GPIO1->DATA = ");
   print_uart0(data);
