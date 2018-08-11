1a9df4c4b2972882cbb4ecd524bbd9ec2a1b3c73
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-10 18:55:21 -0700

    add input to DIR

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 #include "io_headers/lpc13xx_stdio.h"
 
 int main(void) {
+  LPC_GPIO1->DIR |= (1 << 3);
+  // ldr	r2, [pc, #68] : r2 = 0x40012000
   LPC_GPIO1->DATA |= (1 << 2);
+  // orr	r3, r3, #4 : r3 = 0x04
+  // str	r3, [r2] : stores with immediate offset 0x40012000 + 0x04
 
   char *data;
   tochar(LPC_GPIO1->DATA, data);
