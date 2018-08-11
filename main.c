baed7fe1626942448eb1defcc9f32628f1f624e5
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-10 17:26:49 -0700

    clean up function code

--- main.c
+++ main.c
@@ -1,29 +1,5 @@
 #include "io_headers/lpc13xx.h"
-
-volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
-
-void print_uart0(const char *s) {
-  // Loop until end of string
-  while(*s != '\0') {
-    *UART0DR = (unsigned int)(*s); // Transmit char
-    s++; // Next char
-  }
-}
-
-char *tochar(int i, char *p) {
-  if (i / 10 == 0) {
-    // No more digits.
-    *p++ = i + '0';
-    *p = '\0';
-    return p;
-  }
-
-  p = tochar(i / 10, p);
-  *p++ = i % 10 + '0';
-  *p = '\0';
-
-  return p;
-}
+#include "io_headers/lpc13xx_stdio.h"
 
 int main(void) {
   LPC_GPIO1->DATA |= (1 << 2);
