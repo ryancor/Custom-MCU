fbd34ed2db0a7685e5ece01b22f415e1cdc1d47e
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-20 02:16:44 -0700

    support for user input

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "io_headers/lpc13xx.h"
 #include "io_headers/lpc13xx_stdio.h"
+#include "io_headers/lpc13xx_mcu_map.h"
+#include "stdio.h"
 
 int main(void) {
   GPIO1_DIR->DIR |= (1 << 2); // set pin to output
@@ -12,13 +14,20 @@ int main(void) {
   print_uart0(data);
   print_uart0("\n");
 
+  char* user_input;
+
   while(1) {
+    mcu_layout();
+
+    print_uart0("Enter Command-> ");
+    gets(user_input);
+
     if(strcmp_s(data, "1") == 0) {
       print_uart0("LED On\n");
     } else {
       print_uart0("LED Off\n");
     }
-    delay(900000);
+    delay(990000);
   }
   return 0;
 }
