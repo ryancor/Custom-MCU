c163f8815c8b681caccee28d7fa4c9f6ba576d5c
Ryan Cornateanu <ryan_cornateanu@yahoo.com>
2018-08-20 11:10:21 -0700

    ascii sim of pin sck_13 high

--- io_headers/lpc13xx_mcu_map.h
+++ io_headers/lpc13xx_mcu_map.h
@@ -28,3 +28,25 @@ void mcu_layout() {
   print_uart0(" \\                         /          \n");
   print_uart0("  ------------------------- \n\n");
 }
+
+void mcu_sck_13_high() {
+  print_uart0("                              +-----+\n");
+  print_uart0("+----[PWR]--------------------| USB |--+\n");
+  print_uart0("|                             +-----+  |\n");
+  print_uart0("|          GND/RST2 [ ][ ]             |\n");
+  print_uart0("|        MOSI2/SCK2 [ ][ ]   A5/SCL[ ] |\n");
+  print_uart0("|                              AREF[ ] |\n");
+  print_uart0("|                               GND[ ] |\n");
+  print_uart0("| [ ]N/C                     SCK/13[H] |\n");
+  print_uart0("| [ ]v.ref                  MISO/12[ ] |\n");
+  print_uart0("| [ ]RST                    MOSI/11[ ] |\n");
+  print_uart0("|               ....                   |\n");
+  print_uart0("|                                      |\n");
+  print_uart0("| [ ]A4/SDA    RST SCK MISO    TX>1[ ] |\n");
+  print_uart0("| [ ]A5/SCL    [ ] [ ] [ ]     RX<0[ ] |\n");
+  print_uart0("|              [ ] [ ] [ ]             |\n");
+  print_uart0("|               |   |   |             /\n");
+  print_uart0("|    LPC13     GND MOSI 5V   --------- \n");
+  print_uart0(" \\                         /          \n");
+  print_uart0("  ------------------------- \n\n");
+}
